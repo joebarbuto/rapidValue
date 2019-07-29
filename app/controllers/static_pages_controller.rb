4fa31a1afb4ab0bbc2f6b112f63b2b324f089a98
jbarbuto <nygfan992@gmail.com>
2019-07-29 12:56:22 -0400

    Add Static Pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def core
+  end
+
+  def ws_approach
+  end
+
+  def enable
+  end
+
+  def resources
+  end
+
+  def case_studies
+  end
+
+  def play
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
+  get 'static_pages/core'
+
+  get 'static_pages/ws_approach'
+
+  get 'static_pages/enable'
+
+  get 'static_pages/resources'
+
+  get 'static_pages/case_studies'
+
+  get 'static_pages/play'
+
   root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
