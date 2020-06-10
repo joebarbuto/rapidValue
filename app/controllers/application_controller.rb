6e6b3f2d7c92415dcef0165c39f19b3027966969
jbarbuto <nygfan992@gmail.com>
2020-06-10 11:38:18 -0400

    Redirect attempt 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ end
 before_action :redirect_subdomain
 
 def redirect_subdomain
-  if request.host == 'mysterious-lowlands-53435.herokuapp.com' || request.host == 'engagementresources.vpcxazureasena2.na.ase.jnj.com/' || request.host == 'engagementresources.vpcxazureasena2.na.ase.jnj.com/'
+  if request.host == 'mysterious-lowlands-53435.herokuapp.com' || request.host == 'engagementresources.vpcxazureasena2.na.ase.jnj.com' || request.host == 'www.engagementresources.vpcxazureasena2.na.ase.jnj.com'
     redirect_to 'https://agile.jnj.com', :status => 301
   end
 end
