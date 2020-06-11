19206406ab13f094f8361bcae486866842a846b1
jbarbuto <nygfan992@gmail.com>
2020-06-11 10:57:05 -0400

    Flash try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ before_action :redirect_subdomain
 
 def redirect_subdomain
   if request.host == 'mysterious-lowlands-53435.herokuapp.com' || request.host == 'engagementresources.vpcxazureasena2.na.ase.jnj.com' || request.host == 'www.engagementresources.vpcxazureasena2.na.ase.jnj.com'
-    redirect_to 'https://agile.jnj.com', :status => 301
+    redirect_to 'http://batdev.vpcxazureasena2.na.ase.jnj.com/', :status => 301, :flash => { :success => "The ER has been transitioned into the Business Agile Toolbox (BAT)" }
   end
 end
 
